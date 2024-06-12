53553732fec76273c9ffc576c54627bd5158a89e
Ralph Scronce <scronce@comcast.net>
2024-06-12 17:09:18 -0400

    chore: update root route response message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!!! This is my first AWS project.'));
+app.get('/', (req, res) => res.send('Hello World!!! This is my first AWS Elastic Beanstalk project.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
